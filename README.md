630911229d8e33a34cd754efa7d4d8fb1b518272
SlimeCodex <ale.nicolini.p@gmail.com>
2023-10-01 06:12:18 -0300

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# SmartSyncEvent
-Thread-safe event management for synchronous tasks.
+# SmartSyncEvent Library
+
+The `SmartSyncEvent` library provides a convenient way to manage timed events in Arduino and ESP32 projects, enabling you to check whether a certain amount of time has passed since the last event trigger.
+
+## Features
+
+- **Automatic Instance Management**: Manages unique event instances based on the file and line of invocation.
+- **Thread Safety**: Provides thread-safe handling for ESP32 projects.
+- **Convenience**: Utilize through a simple macro for triggering events.
+
+## Installation
+
+1. Download the library as a zip file.
+2. Open your Arduino IDE or PlatformIO.
+3. Add the library via the respective "Add Library" feature.
+
+## Usage
+
+### Including the Library
+
+```cpp
+#include "SmartSyncEvent.h"
+```
+
+### Triggering an Event
+
+Utilize the SYNC_EVENT(ms) macro. This returns a Result struct, which can be implicitly cast to bool to verify if the event was triggered. Moreover, the event's unique ID can be accessed with the .eventID member.
+
+```cpp
+if (SYNC_EVENT(500)) {
+    // This block will execute every 500ms.
+}
+```
+
+## Manually Resetting an Event Timer
+
+For manual timer resets of any sync event, use its unique ID.
+
+```cpp
+SmartSyncEvent::reset(eventID);
+```
+
+## Retrieving the Event ID Directly
+
+Directly obtain a unique ID for an event:
+
+```cpp
+auto result = SYNC_EVENT(1000);
+unsigned int id = result.eventID;
+```
+
+## License
+
+Distributed under the MIT License. See the LICENSE file for more information.
--- src/SmartSyncEvent.cpp
+++ src/SmartSyncEvent.cpp
@@ -23,67 +23,67 @@
 std::map<unsigned int, unsigned long> SmartSyncEvent::id_to_timer;
 
 #ifdef ESP32
-    SemaphoreHandle_t SmartSyncEvent::mutex = xSemaphoreCreateMutex();
+	SemaphoreHandle_t SmartSyncEvent::mutex = xSemaphoreCreateMutex();
 #endif
 
 SmartSyncEvent::SmartSyncEvent() {
 }
 
 SmartSyncEvent::~SmartSyncEvent() {
-    #ifdef ESP32
-        vSemaphoreDelete(mutex); // Cleanup for ESP32
-    #endif
+	#ifdef ESP32
+		vSemaphoreDelete(mutex); // Cleanup for ESP32
+	#endif
 }
 
 unsigned int SmartSyncEvent::get_id(const std::string& file, int line) {
-    unsigned int hash = 0;
-    for (char c : file) {
-        hash = (hash * 31) + c;
-    }
-    return hash + line;
+	unsigned int hash = 0;
+	for (char c : file) {
+		hash = (hash * 31) + c;
+	}
+	return hash + line;
 }
 
 bool SmartSyncEvent::trigger_id(int ms, unsigned int eventID) {
-    #ifdef ESP32
-        xSemaphoreTake(mutex, portMAX_DELAY);
-    #endif
+	#ifdef ESP32
+		xSemaphoreTake(mutex, portMAX_DELAY);
+	#endif
 
-    unsigned long& timer = id_to_timer[eventID];
+	unsigned long& timer = id_to_timer[eventID];
 
-    if (id_to_timer.size() > MAX_INSTANCES) {
-        #ifdef ESP32
-            xSemaphoreGive(mutex);
-        #endif
-        return false; // Too many unique sync events
-    }
+	if (id_to_timer.size() > MAX_INSTANCES) {
+		#ifdef ESP32
+			xSemaphoreGive(mutex);
+		#endif
+		return false; // Too many unique sync events
+	}
 
-    bool shouldTrigger = (millis() - timer > ms);
-    if (shouldTrigger) {
-        timer = millis();
-    }
+	bool shouldTrigger = (millis() - timer > ms);
+	if (shouldTrigger) {
+		timer = millis();
+	}
 
-    #ifdef ESP32
-        xSemaphoreGive(mutex);
-    #endif
-    return shouldTrigger;
+	#ifdef ESP32
+		xSemaphoreGive(mutex);
+	#endif
+	return shouldTrigger;
 }
 
 SmartSyncEvent::Result SmartSyncEvent::trigger(int ms, const std::string& file, int line) {
-    unsigned int eventID = get_id(file, line);
-    bool wasTriggered = trigger_id(ms, eventID);
-    return {wasTriggered, eventID};
+	unsigned int eventID = get_id(file, line);
+	bool wasTriggered = trigger_id(ms, eventID);
+	return {wasTriggered, eventID};
 }
 
 void SmartSyncEvent::reset(unsigned int eventID) {
-    #ifdef ESP32
-        xSemaphoreTake(mutex, portMAX_DELAY);
-    #endif
+	#ifdef ESP32
+		xSemaphoreTake(mutex, portMAX_DELAY);
+	#endif
 
-    if (id_to_timer.find(eventID) != id_to_timer.end()) {
-        id_to_timer[eventID] = millis();
-    }
+	if (id_to_timer.find(eventID) != id_to_timer.end()) {
+		id_to_timer[eventID] = millis();
+	}
 
-    #ifdef ESP32
-        xSemaphoreGive(mutex);
-    #endif
+	#ifdef ESP32
+		xSemaphoreGive(mutex);
+	#endif
 }
